01a0e9903c854e5c0d7c43cc72a5cb84ee4d4388
Mason McCully <mmccully2000@gmail.com>
2018-02-16 14:19:30 -0600

    nn

--- src/server/ShipActor.java
+++ src/server/ShipActor.java
@@ -1,11 +1,30 @@
 package server;
 
+import mayflower.World;
+
 public class ShipActor extends SpaceObject{
     private SpaceCannon cannon;
     private EngineerSystem engie;
 
     public ShipActor(String img) {
         super(img);
+        engie = new EngineerSystem();
+        cannon = new SpaceCannon("rsrc/LaserCannon.png");
+    }
+
+    public SpaceCannon getCannon() {
+        return cannon;
+    }
+
+    public EngineerSystem getEngie() {
+        return engie;
+    }
+
+    @Override
+    protected void addedToWorld(World world) {
+        super.addedToWorld(world);
+        world.addObject(cannon, getX(), getY());
+        world.addObject(engie, getX(), getY());
     }
 
     @Override
