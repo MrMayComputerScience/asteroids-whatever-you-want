db4d4aa14166b22fbe9c6fe33e951080b694c56a
Mason McCully <mmccully2000@gmail.com>
2018-03-02 13:56:40 -0600

    Asteroids explode when hit by lasers

--- src/server/SpaceLaser.java
+++ src/server/SpaceLaser.java
@@ -1,5 +1,7 @@
 package server;
 
+import mayflower.Actor;
+
 public class SpaceLaser extends SpaceObject {
     public SpaceLaser() {
         super("rsrc/Laser.png");
@@ -8,5 +10,16 @@ public class SpaceLaser extends SpaceObject {
         return String.format("lazer:%d %d %d %d", getId(), getX(), getY(), getRotation());
 //        return String.format("lazer:%d %d %d", getX(), getY(), getRotation());
 
+    }
+    public void act(){
+        for(Actor a : getIntersectingObjects(Actor.class)){
+            if(a instanceof Explodable){
+                Explodable e = (Explodable)a;
+                e.explode();
+                if(getWorld() != null)
+                    getWorld().removeObject(this);
+            }
+
+        }
     }
 }
--- src/server/Vector.java
+++ src/server/Vector.java
@@ -4,6 +4,18 @@ public class Vector {
     private double x;
     private double y;
     public static final Vector ZERO = new Vector(0,0);
+
+    public static void main(String[] args){
+        Vector v = Vector.fromMagAndAngle(10,0);
+        System.out.println("v = " + v);
+        v = fromMagAndAngle(10,45);
+        System.out.println("v = " + v);
+        System.out.println("v.magnitude() = " + v.magnitude());
+        v = fromMagAndAngle(10, -90);
+        System.out.println("v = " + v);
+        System.out.println("v.magnitude() = " + v.magnitude());
+    }
+
     public Vector(double x, double y){
         this.x = x;
         this.y = y;
@@ -25,7 +37,21 @@ public class Vector {
     public double dot(Vector other){
         return x * other.getX() + y * other.getY();
     }
+    public Vector scale(double scalar){
+        return new Vector(x * scalar, y * scalar);
+    }
     public double magnitude(){
         return Math.sqrt(x*x + y*y);
+    }
+    public static Vector fromMagAndAngle(double magnitude, double angle){
+        Vector ret;
+        double angleRads = angle * Math.PI/180;
+        double x = magnitude * Math.cos(angleRads);
+        double y = magnitude * Math.sin(angleRads);
+        ret = new Vector(x,y);
+        return ret;
+    }
+    public String toString(){
+        return "<"+x+", "+y+">";
     }
 }
